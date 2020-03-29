16acbe187b57114a7d98aecca2e17cb3e8e3e153
gabrieldasneves <galvesdasneves@gmail.com>
2020-03-29 18:41:17 +0200

    preparando o servidor

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* 
-para printar no terminal
+função pronta para printar no terminal
 console.log("hello world");
 */
 
@@ -19,6 +19,32 @@ basico de javascript:
 
         soma(1,2);
 
-
+    objetos:
+        const xicara = {
+            cor: white,
+            tamanho: 10,
+            receber(){
+                <logica>
+            }
+        }
+        console.log(xicara);
+        console.log(xicara.cor);
 */
 
+// usando o express para criar e configurar o servidor
+const express = require("express") // dizendo que vou usar o express
+const server = express()
+
+// criando uma rota "/"  e captura o pedido de clientepara responder
+server.get("/", function(request, response){
+    return response.sendFile(__dirname + "/index.html")
+})
+
+// ligando o servidor na porta 3000
+server.listen(3000)
+
+/*
+para evitar ficar iniciando o servidor toda hora, 
+baixe o nodemon (npm i nodemon)
+e, no arquivo server.js, troque o start pelo nodemon server.js
+*/
