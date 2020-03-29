4f98f4ef0a39d3ae7043c4ab8b7b664f1be850e5
gabrieldasneves <galvesdasneves@gmail.com>
2020-03-29 17:50:12 +0200

    preparando o servidor

---
+++ server.js
@@ -0,0 +1,24 @@
+/* 
+para printar no terminal
+console.log("hello world");
+*/
+
+/*
+basico de javascript:
+    variaveis:
+        const mensagem = "oi" // string
+        console.log(mensagem);
+
+        cons number = 15 //int
+        console.log(number);
+    
+    funções:
+        function soma(numero1, numero2){
+            console.log(numero1 + numero2);
+        }
+
+        soma(1,2);
+
+
+*/
+
